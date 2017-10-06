6a9083810329775574c0eb1f7d1d94712b0bf8f6
Cedric Michaux <cedric@he8us.be>
2017-10-06 10:05:00 +0200

    Added SQLite connection string support

--- test/database-config_spec.js
+++ test/database-config_spec.js
@@ -22,7 +22,7 @@ describe('Database Configuration', function () {
             should(resultObject).be.deepEqual(testObject);
         });
 
-        it('converts a basic connection string to a connection object', function () {
+        it('converts a mysql connection string to a connection object', function () {
             var testObject = {
                 connection: 'mysql://user:password@127.0.0.1:12345/ghost'
             }
@@ -38,7 +38,7 @@ describe('Database Configuration', function () {
             });
         });
 
-        it('converts a connection string to a connection object with an optional port', function () {
+        it('converts a mysql connection string to a connection object with an optional port', function () {
             var testObject = {
                 connection: 'mysql://user:password@127.0.0.1/ghost'
             }
@@ -53,7 +53,7 @@ describe('Database Configuration', function () {
             });
         });
 
-        it('converts a connection string with the charset defined to a connection object', function () {
+        it('converts a mysql connection string with the charset defined to a connection object', function () {
             var testObject = {
                 connection: 'mysql://user:password@127.0.0.1:12345/ghost?characterEncoding=utf8'
             }
@@ -67,6 +67,42 @@ describe('Database Configuration', function () {
                 charset: 'utf8',
                 port: '12345',
                 database: 'ghost'
+            });
+        });
+
+        it('converts a sqlite connection string', function () {
+            var testObject = {
+                connection: 'sqlite:file:/home/user/database.db'
+            }
+
+            var resultObject = config.ensureObject(testObject);
+
+            should(resultObject.connection).be.deepEqual({
+                filename: "/home/user/database.db"
+            });
+        });
+
+        it('converts a sqlite connection string on windows', function () {
+            var testObject = {
+                connection: 'sqlite:file:///C:/Documents%20and%20Settings/user/Desktop/database.db'
+            }
+
+            var resultObject = config.ensureObject(testObject);
+
+            should(resultObject.connection).be.deepEqual({
+                filename: "/C:/Documents%20and%20Settings/user/Desktop/database.db"
+            });
+        });
+
+        it('converts a sqlite connection string with the file protocol omitted', function () {
+            var testObject = {
+                connection: 'sqlite:/home/user/database.db'
+            }
+
+            var resultObject = config.ensureObject(testObject);
+
+            should(resultObject.connection).be.deepEqual({
+                filename: "/home/user/database.db"
             });
         });
     });
